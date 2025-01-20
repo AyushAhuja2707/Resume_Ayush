03fb55b1447e784ae656cc0238635798426aa77e
AyushAhuja2707 <ayushahuja200@gmail.com>
2025-01-21 03:27:29 +0530

    Updates

---
+++ js/first.js
@@ -0,0 +1,58 @@
+console.log("First Js");
+
+var x = 10;
+console.log("X --> "+x);
+
+var y = 20;
+console.log("Y --> "+y);
+
+var z = x+y;
+console.log("Z --> "+z);
+
+// alert("check");
+
+
+let name = "John";
+const age = 30;
+
+var isMarried = true;
+
+console.log(name);
+console.log(age);
+console.log(isMarried);
+
+
+name = "Jane";
+// age=50;
+
+if (true) {
+  var value = 10;
+  const isActive = true;
+  console.log("Hello World " + value);
+  console.log("Hello World " + isActive);
+}
+
+console.log("Hello World " + value);
+
+
+
+// javascript is dynamic type language:
+// static type concpet:
+
+// primitive types: number, string, boolean, null, undefined
+let x1 = 10;
+let x2 = "hello";
+let x3 = true;
+
+let x4 = null;
+let x5 = undefined;
+let x6;
+
+let course;
+
+
+let value3= Number.MAX_SAFE_INTEGER+10;
+console.log(value3);
+console.log(typeof value3)
+
+
